bf718dd3f996d0123e9601516a91aa20f2d99581
Mario.Vega003 <Mario.Vega003@2129-B1C-350304.mykendall.mymdc.net>
2019-11-18 19:37:42 -0500

    created menu

--- src/newdb/dbCon.java
+++ src/newdb/dbCon.java
@@ -5,6 +5,7 @@
 package newdb;
 
 import java.sql.*;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +14,11 @@ import java.sql.*;
 
 
 public class dbCon {
+    
+    Scanner scan = new Scanner(System.in);
+    boolean flag = true;
+    int userInput = 0;
+    
 
     public void runMe(String host, String database, String user, String password)
             throws Exception {
@@ -25,6 +31,36 @@ public class dbCon {
 
         /* now run a select query of the intended database */
         exeQuery(conMe, "SELECT * FROM Students");
+        
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println("Enter 1 to show the class roster.");
+            System.out.println("Enter 2 to select a student by ID number.");
+            System.out.println("Enter 3 to add a student to the roster.");
+            System.out.println("Enter 4 to update a students information.");
+            System.out.println("Enter 5 to quit.");
+            
+            userInput = scan.nextInt();
+            
+            switch(userInput){
+                case 1:
+                    exeQuery(conMe, "SELECT * FROM Students");
+                    break;
+                case 2:
+                    System.out.println("Enter ID number");
+                    break;
+                case 3:
+                    System.out.println("Add student");
+                    break;
+                case 4:
+                    System.out.println("Update student");
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+            }
+            
+        }while(flag);
 
         /* close the database */
         conMe.close();
@@ -36,6 +72,7 @@ public class dbCon {
             
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("MySQL Driver Found");
+            System.out.println("Welcome");
         } catch (java.lang.ClassNotFoundException e) {
             System.out.println("MySQL JDBC Driver not found ... ");
             throw (e);
@@ -70,7 +107,8 @@ public class dbCon {
                                 String name = sqls.getString("studentName");
                                 int age = sqls.getInt("age");
                                 double gpa = sqls.getDouble("gpa");
-                                System.out.println("Student ID:" + id + " Student Name:" + name + " Student Age:" + age + " Student GPA:" + gpa);
+                                System.out.println("Student Details");
+                                System.out.println("ID:" + id + " Name:" + name + " Age:" + age + " GPA:" + gpa);
                         }
 
                         sqls.close();
@@ -85,8 +123,8 @@ public class dbCon {
         
             dbCon a = new dbCon();
             a.runMe("dbjava.c96ahlzpethg.us-east-1.rds.amazonaws.com", "dbjava", "dbjava", "mypassword");
-            
-          System.out.println ("usage: java dbCon host database user passwd");
+           
+            System.out.println ("usage: java dbCon host database user passwd");
         }
     }
 
